e39a5b87ca1914c43350c93a28a153f72eea8c48
Hennoshin <m.irfan.hilmi@gmail.com>
2022-02-09 12:30:58 +0700

    Add automatic password hash and save to database

--- src/java/Model/User.java
+++ src/java/Model/User.java
@@ -5,6 +5,19 @@
  */
 package Model;
 
+import Database.DB;
+import java.lang.Math;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Base64;
+import java.util.HashMap;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
 /**
  *
  * @author Asus
@@ -14,7 +27,40 @@ public class User implements IModel {
     private String password;
     private String name;
     private String address;
+    
+    private boolean dirty;
 
+    public User(ResultSet rs) throws SQLException {
+        this.email = rs.getString("email");
+        this.password = rs.getString("password");
+        this.name = rs.getString("name");
+        this.address = rs.getString("address");
+        
+        this.dirty = false;
+    }
+
+    public User(String email, String password, String name, String address) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        this.email = email;
+        this.setPassword(password);
+        this.name = name;
+        this.address = address;
+        
+        this.dirty = true;
+    }
+    
+    
+    public static User find(String email) {
+        try {
+            ResultSet res = DB.getConnect().where("usertbl", "email", email);
+            res.first();
+            
+            return new User(res);
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+    
     public String getEmail() {
         return email;
     }
@@ -27,8 +73,13 @@ public class User implements IModel {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword(String password) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), new byte[]{0}, 65536, 128);
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = f.generateSecret(spec).getEncoded();
+        Base64.Encoder enc = Base64.getEncoder();
+        
+        this.password = enc.encodeToString(hash);
     }
 
     public String getName() {
@@ -47,9 +98,30 @@ public class User implements IModel {
         this.address = address;
     }
     
+    public boolean comparePassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), new byte[]{0}, 65536, 128);
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = f.generateSecret(spec).getEncoded();
+        Base64.Encoder enc = Base64.getEncoder();
+        
+        String passhash = enc.encodeToString(hash);
+        return passhash.equals(this.password);
+    }
+    
     @Override
-    public boolean save() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean save() throws SQLException {
+        DB db = DB.getConnect();
+        if (dirty) {
+            db.insert("usertbl", new int[]{Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR}, this.email, this.password, this.name, this.address);
+        }
+        else {
+            HashMap<String, Object> pkey = new HashMap();
+            pkey.put("email", this.email);
+            db.update("usertbl", pkey, new String[]{"email", "password", "name", "address"}, new int[]{Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR},
+                    this.email, this.password, this.name, this.address);
+        }
+        
+        return true;
     }
 
     @Override
